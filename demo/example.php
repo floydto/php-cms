f378ca4a84e8483a21c9a3cdf4ad4b543ab25fef
Floyd To <69040807+floydto@users.noreply.github.com>
2021-12-12 16:15:35 +0800

    init

---
+++ demo/example.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+<?php
+    $title = "asdasdasd";
+    $testing = "123";
+?>
+
+<h1>
+    <?php echo $title ?>
+    <?php echo $testing ?>
+    <?php echo $testing ?>
+    <?php echo $testing ?>
+</h1>
+
+</html>
